4125405854af9764f2fd8bbf254594d0953adba5
KonradRudnicki <97458824+KonradRudnicki@users.noreply.github.com>
2022-01-15 16:06:40 +0100

    wprowadzenie strony bez tla

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,73 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <header>
+              <img class="circle" src="images/avatar.jpg" width="150px" height="150px"></img>
+              <div>
+                  <h1>Konrad Rudnicki</h1>
+                  <span>Student</span>
+              </div>
+              <address>
+                  <ul>
+                      <li><img class="address-icon" src="images/tel.png"/><b>Numer telefonu</b>: 662 582 338</li>
+                      <li><img class="address-icon" src="images/mail.png"/><b>Email</b>: konradrudnicki0@gmail.com</li>
+                      <li><img class="address-icon" src="images/uro.png"/><b>Data urodzenia</b>: 24.05.2002</li>
+                      <li><img class="address-icon" src="images/miejsce.png"/><b>Miejsce urodzenia</b>: Wrocław</li>
+                  </ul>
+              </address>
+              </header>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+          <main>
+              <section>
+                  <h2>O mnie</h2>
+                  <p>
+                      Mam na imię <b>Konrad</b>! Jestem studentem pierwszego roku informatyki
+                      stosowanej na <a href="https://pwr.edu.pl/">Politechince Wrocławskiej</a>. Wcześniej ukończyłem
+                      Liceum Ogólnokształcące nr 5 we Wrocławiu.
+                      Cechuję się kreatywnością, otwartością oraz logicznym myśleniem.
+                      Jestem ambitną osobą, która wytrwale walczy o sukcesy.
 
-      <Footer />
+                  </p>
+              </section>
+
+              <section>
+                  <h2>Doświadczenie zawodowe</h2>
+                  <ul>
+                      <li>Projekt strony internetowej</li>
+                      <li>Projekt zaprogramowania silnika trójfazowego</li>
+                  </ul>
+              </section>
+
+              <section>
+                  <h2>Języki</h2>
+                  <ul>
+                      <li>Polski - ojczysty</li>
+                      <li>Angielski - B2</li>
+                      <li>Niemiecki - A1</li>
+                  </ul>
+              </section>
+
+              <section>
+                  <h2>Wykształcenie</h2>
+                  <ul>
+                      <li>2015-2018: Gimnazjum nr 23 we Wrocławiu - profil mat</li>
+                      <li>2018-2021: <a href="https://lo5.wroc.pl/">
+                          Liceum Ogólnokształcące nr 5 we Wrocławiu</a>
+                          - profil mat-fiz-inf</li>
+      		<li>2021- : Politechnika Wrocławska - Informatyka Stosowana w języku angielskim</li>
+                  </ul>
+              </section>
+
+              <section>
+              <h2>Zainteresowania</h2>
+                  <ul>
+                      <li>Motoryzacja</li>
+                      <li>Kulturystyka</li>
+                      <li>Piłka nożna</li>
+                      <li>Sztuki walki</li>
+                  </ul>
+              </section>
+          </main>
     </div>
   )
 }
